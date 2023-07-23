2eb80d21205be885a88c1bf918b6637271f62659
Neeky <1721900707@qq.com>
2023-07-23 22:16:37 +0800

    ✨feature: 调整自动变更的名字

--- src/pthreads/join.cpp
+++ src/pthreads/join.cpp
@@ -18,19 +18,19 @@ thread_fun(void *arg)
 int main()
 {
     int err;
-    pthread_t tid1, tid2;
+    pthread_t tid;
     void *tret;
 
     /**
      * 创建一个子线程，在子线程执行完成之后，职得子线程上堆上保存的值，并打印+回收堆上的空间
      */
     // 1. 创建线程
-    err = pthread_create(&tid1, NULL, thread_fun, NULL);
+    err = pthread_create(&tid, NULL, thread_fun, NULL);
     if (err != 0)
         perror("can't create thread fail . \n");
 
     // 2. join 的时候并收回值
-    err = pthread_join(tid1, &tret);
+    err = pthread_join(tid, &tret);
     if (err != 0)
         perror("join thread fail . \n");
     printf("thread joined, main thread gots return value = %d. \n", *(int *)tret);
